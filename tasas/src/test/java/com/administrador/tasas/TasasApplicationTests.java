b593960a2fcdc92b058fde375bb318a9f1cef22f
Lizeth Ovando Velasquez <lovandov@grupobisa.com>
2024-01-06 06:12:59 -0400

    Initialize project

---
+++ tasas/src/test/java/com/administrador/tasas/TasasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.administrador.tasas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
